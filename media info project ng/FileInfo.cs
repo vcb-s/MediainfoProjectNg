a5b3f67b1ed8b40d1df965e7b20f3574c19c5fb6
Mark Wu <wfwf1997@163.com>
2017-05-05 23:02:05 +0800

    Initial commit

---
+++ media info project ng/FileInfo.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using media_info_project_ng.Annotations;
+using MediaInfoLib;
+
+namespace media_info_project_ng
+{
+    public class GeneralInfo
+    {
+        public string Filename { get; set; }
+        public string Format { get; set; }
+        public string Bitrate { get; set; }
+    }
+
+    public class VideoInfo
+    {
+        public int TrackId { get; set; }
+        public string Fps { get; set; }
+        public string Bitrate { get; set; }
+    }
+
+    public class AudioInfo
+    {
+        public int TrackId { get; set; }
+        public string Format { get; set; }
+        public string BitDepth { get; set; }
+        public string Bitrate { get; set; }
+    }
+
+    public class FileInfoDetail
+    {
+        public string Filename { get; set; }
+        public string Fps { get; set; }
+        public string Format1 { get; set; }
+        public string Format2 { get; set; }
+    }
+
+    public class FileInfo
+    {
+        private GeneralInfo GeneralInfo { get;} = new GeneralInfo();
+        private List<VideoInfo> VideoInfos { get;} = new List<VideoInfo>();
+        private List<AudioInfo> AudioInfos { get;} = new List<AudioInfo>();
+        public string Summary { get;}
+
+        // TODO: Is it better to use property than method?
+        public FileInfoDetail GetDetail() => new FileInfoDetail
+        {
+            Filename = GeneralInfo.Filename,
+            Fps = VideoInfos[0]?.Fps,
+            // TODO: more elegant
+            Format1 = AudioInfos.Count > 0 ? AudioInfos[0]?.Format : "",
+            Format2 = AudioInfos.Count > 1 ? AudioInfos[1]?.Format : ""
+        };
+
+        public FileInfo(string url)
+        {
+            var MI = new MediaInfo();
+            MI.Open(url);
+            MI.Option("Complete");
+            Summary = MI.Inform();
+
+            {
+                GeneralInfo.Filename = MI.Get(StreamKind.General, 0, "FileName");
+                GeneralInfo.Format = MI.Get(StreamKind.General, 0, "Format");
+                GeneralInfo.Bitrate = MI.Get(StreamKind.General, 0, "OverallBitRate/String");
+            }
+
+            for (var i = 0; i < int.Parse(MI.Get(StreamKind.General, 0, "VideoCount")); i++)
+            {
+                VideoInfos.Add(new VideoInfo
+                {
+                    TrackId = i,
+                    Fps = MI.Get(StreamKind.Video, 0, "FrameRate/String"),
+                    Bitrate = MI.Get(StreamKind.Video, 0, "BitRate")
+                });
+            }
+
+            for (var i = 0; i < int.Parse(MI.Get(StreamKind.General, 0, "AudioCount")); i++)
+            {
+                AudioInfos.Add(new AudioInfo
+                {
+                    TrackId = i,
+                    Format = MI.Get(StreamKind.Audio, i, "Format"),
+                    BitDepth = MI.Get(StreamKind.Audio, i, "BitDepth/String"),
+                    Bitrate = MI.Get(StreamKind.Audio, i, "BitRate") == ""
+                        ? ""
+                        : (int.Parse(MI.Get(StreamKind.Audio, i, "BitRate")) / 1000).ToString()
+                });
+            }
+        }
+    }
+
+    public class FileInfoModel : INotifyPropertyChanged
+    {
+        public List<FileInfo> FileInfos { get; } = new List<FileInfo>();
+
+        public List<FileInfoDetail> FileInfoDetails => FileInfos.Select(fileInfo => fileInfo.GetDetail()).ToList();
+        public int ItemsCount => FileInfoDetails.Count;
+
+        public FileInfoModel()
+        {
+        }
+
+        public FileInfoModel(IEnumerable<string> urls)
+        {
+            FileInfos.AddRange(urls.Select(url => new FileInfo(url)).ToList());
+        }
+
+        public void AddItems(IEnumerable<string> urls)
+        {
+            FileInfos.AddRange(urls.Select(url => new FileInfo(url)).ToList());
+
+            OnPropertyChanged(nameof(FileInfoDetails));
+            OnPropertyChanged(nameof(ItemsCount));
+        }
+
+        public void Clear()
+        {
+            FileInfos.Clear();
+
+            OnPropertyChanged(nameof(FileInfoDetails));
+            OnPropertyChanged(nameof(ItemsCount));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ media info project ng/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Data;
+using MediaInfoLib;
+
+namespace media_info_project_ng
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly FileInfoModel _fileInfoModel = new FileInfoModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+//            DataGrid1.ItemsSource = _fileInfoModel.FileInfoDetails;
+            DataGrid1.DataContext = _fileInfoModel;
+            StatusBlock.DataContext = _fileInfoModel;
+//            var MI = new MediaInfo();
+//            var toDisplay = MI.Option("Info_Version");
+//            if (toDisplay.Length == 0)
+//            {
+//                toDisplay = "MediaInfo.Dll: this version of the DLL is not compatible";
+//            }
+//            StatusBlock.Text = toDisplay;
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            var sw = new Stopwatch();
+            var toDisplay = string.Empty;
+
+            sw.Start();
+
+            _fileInfoModel.AddItems(new List<string>
+            {
+                @"D:\Hanasaku\[Liuyun&VCB-S]HanaSaku Iroha[18][Hi10p_1080p][BDRip][x264_flac_ac3].mkv",
+                @"D:\Videos\[HorribleSubs] Zero kara Hajimeru Mahou no Sho - 04 [1080p].mkv",
+                @"D:\LLSS67\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [Ma10p_1080p]\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [10][Ma10p_1080p][x265_flac].mkv",
+                @"D:\LLSS67\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [Ma10p_1080p]\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [11][Ma10p_1080p][x265_flac].mkv",
+                @"D:\LLSS67\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [Ma10p_1080p]\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [12][Ma10p_1080p][x265_flac].mkv",
+                @"D:\LLSS67\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [Ma10p_1080p]\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [13][Ma10p_1080p][x265_flac].mkv",
+                @"D:\LLSS\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [Ma10p_1080p]\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [01v2][Ma10p_1080p][x265_flac].mkv",
+                @"D:\LLSS\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [Ma10p_1080p]\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [02][Ma10p_1080p][x265_flac].mkv",
+                @"D:\LLSS\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [Ma10p_1080p]\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [03][Ma10p_1080p][x265_flac].mkv",
+                @"D:\LLSS\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [Ma10p_1080p]\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [04][Ma10p_1080p][x265_flac].mkv",
+                @"D:\LLSS\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [Ma10p_1080p]\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [05][Ma10p_1080p][x265_flac].mkv",
+                @"D:\LLSS\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [Ma10p_1080p]\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [06][Ma10p_1080p][x265_flac].mkv",
+                @"D:\LLSS\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [Ma10p_1080p]\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [07][Ma10p_1080p][x265_flac].mkv",
+                @"D:\LLSS\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [Ma10p_1080p]\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [08][Ma10p_1080p][x265_flac].mkv",
+                @"D:\LLSS\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [Ma10p_1080p]\[Nyamazing&VCB-Studio] LoveLive! Sunshine!! [09][Ma10p_1080p][x265_flac].mkv",
+            });
+
+            sw.Stop();
+
+            toDisplay += $"Elapsed {sw.ElapsedMilliseconds}ms\r\n";
+
+            toDisplay += $"Count: {_fileInfoModel.ItemsCount}\r\n";
+
+            TxtBox.Text = toDisplay;
+//            var binding = new Binding {Source = $"列表中已有 {_fileInfoModel.ItemsCount} 个项目"};
+//            StatusBlock.SetBinding(TextBlock.TextProperty, binding);
+        }
+
+
+        private void DataGrid1_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                TxtBox.Text = _fileInfoModel.FileInfos[DataGrid1.SelectedIndex].Summary;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                TxtBox.Text = "";
+            }
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            _fileInfoModel.Clear();
+        }
+
+        private void DataGrid1_OnDrop(object sender, DragEventArgs e)
+        {
+            
+                var paths = new List<string>(e.Data.GetData(DataFormats.FileDrop) as string[]);
+                var FA = paths?.Select(path => File.GetAttributes(path)).ToList();
+            
+            
+        }
+
+        private void DataGrid1_OnDragEnter(object sender, DragEventArgs e)
+        {
+            // TODO: It doesn't work!!!!
+            e.Effects = e.Data.GetDataPresent(DataFormats.FileDrop) ? DragDropEffects.All : DragDropEffects.None;
+        }
+    }
+}
